fb77a84053c2c6d0a7b21a335cd771cff590efc9
Станислава Хоменко <stanisavva@MacBook-Air-Stanislava.local>
2022-09-16 18:02:15 +0300

    Adding Page Object files and a test script

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,5 +3,5 @@ class BasePage():
         self.browser = browser
         self.url = url
 
-   def open(self):
-       self.browser.get(self.url)
+    def open(self):
+        self.browser.get(self.url)
