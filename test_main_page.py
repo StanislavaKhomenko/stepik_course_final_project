2b696e26e0fb00305d3616a5ed5e91766d2dee41
Станислава Хоменко <stanisavva@MacBook-Air-Stanislava.local>
2022-09-16 17:35:41 +0300

    Adding test script

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+   browser.get(link)
+   go_to_login_page(browser)
