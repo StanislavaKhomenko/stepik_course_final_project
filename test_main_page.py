4eecdfef34bd469c76b65f623478a495370cf7a6
Станислава Хоменко <stanisavva@MacBook-Air-Stanislava.local>
2022-09-20 13:02:32 +0300

    Refactor to simplify the code

--- test_main_page.py
+++ test_main_page.py
@@ -7,24 +7,11 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    link = browser.current_url
-    page = LoginPage(browser, link)
-    page.should_be_login_url()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-def test_guest_should_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_form()
-
-def test_guest_should_see_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_register_form()
